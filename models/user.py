0a90e9b6f31c0d09abf628091566366891543df4
ITSNE01 <rabie.haddar1@gmail.com>
2024-12-28 11:26:56 -0800

    User

--- models/user.py
+++ models/user.py
@@ -9,11 +9,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
-    email = Column("email", String(128), nullable=False)
-    password = Column("password", String(128), nullable=False)
-    first_name = Column("first_name", String(128))
-    last_name = Column("last_name", String(128))
-    places = relationship(
-        "Place", backref="user", cascade="all, delete, delete-orphan")
-    reviews = relationship(
-        "Review", backref="user", cascade="all, delete, delete-orphan")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)

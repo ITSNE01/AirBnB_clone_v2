af0f4ee3e9a96f9e398a18707ebf09e2e92c31cb
ITSNE01 <rabie.haddar1@gmail.com>
2024-12-28 11:02:14 -0800

    Cities and states

--- models/state.py
+++ models/state.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-import models
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.city import City
+from sqlalchemy.orm import relationship
 import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name = ""
+    name = Column('name', String(128), nullable=False)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+    cities = relationship(
+        'City', backref="state", cascade="all, delete, delete-orphan")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            city_list = []
-            all_cities = models.storage.all(City).values()
-            for city in all_cities:
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """Return a list of cities with state_id equal to the current State.id"""  # noqa
+            from models import storage
+            from models.city import City
+
+            all_cities = storage.all(City)
+            cities_list = []
+
+            for _, v in all_cities.items():
+                if v.state_id == self.id:
+                    cities_list.append(v)
+
+            return cities_list

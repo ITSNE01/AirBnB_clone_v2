1353d350c102328ed286c3c6c4c07f33338bd1d4
ITSNE01 <rabie.haddar1@gmail.com>
2024-12-28 10:51:25 -0800

    user.py

--- models/user.py
+++ models/user.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        # user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship('Place', backref='user', cascade='delete')
-        # places = relationship('Place', backref='user', cascade='delete', primaryjoin="User.id == Place.user_id")
-        reviews = relationship('Review', backref='user', cascade='delete')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128))
+    last_name = Column("last_name", String(128))
+    places = relationship(
+        "Place", backref="user", cascade="all, delete, delete-orphan")
+    reviews = relationship(
+        "Review", backref="user", cascade="all, delete, delete-orphan")

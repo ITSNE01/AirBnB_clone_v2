68036e650df1ff0e27b08c6c1b1e5cb5ca2b0f0e
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-09 10:04:24 -0800

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,6 +2,7 @@
 """ A script that starts a Flask web application """
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -38,7 +39,7 @@ def number_page(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
+def number_template(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
